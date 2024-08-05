c549622d15a8100899e65d2aa137a143b205529c
Novampr <limedev8756@gmail.com>
2024-08-05 13:08:49 +0100

    DL Potato tests

--- common/src/main/java/org/vivecraft/client_vr/VRState.java
+++ common/src/main/java/org/vivecraft/client_vr/VRState.java
@@ -40,13 +40,6 @@ public class VRState {
             vrInitialized = true;
             ClientDataHolderVR dh = ClientDataHolderVR.getInstance();
             if (dh.vrSettings.stereoProviderPluginID == VRSettings.VRProvider.OPENVR) {
-                // make sure the lwjgl version is the right one
-                // TODO: move this into the init, does mean all callocs need to be done later
-                // check that the right lwjgl version is loaded that we ship the openvr part of
-                if (!Version.getVersion().startsWith("3.3.2")) {
-                    throw new RenderConfigException("VR Init Error", Component.translatable("vivecraft.messages.rendersetupfailed", I18n.get("vivecraft.messages.invalidlwjgl", Version.getVersion(), "3.3.2"), "OpenVR_LWJGL"));
-                }
-
                 dh.vr = new MCOpenVR(Minecraft.getInstance(), dh);
             } else {
                 dh.vr = new NullVR(Minecraft.getInstance(), dh);
@@ -113,6 +106,7 @@ public class VRState {
         } catch (RenderConfigException renderConfigException) {
             vrEnabled = false;
             destroyVR(true);
+            renderConfigException.printStackTrace();
             Minecraft.getInstance().setScreen(new ErrorScreen(renderConfigException.title, renderConfigException.error));
         } catch (Throwable e) {
             vrEnabled = false;
