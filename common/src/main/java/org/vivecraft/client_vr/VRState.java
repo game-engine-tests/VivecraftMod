421b3c175bb583609db5ae2358948c91a5508f31
Novampr <limedev8756@gmail.com>
2024-08-12 23:17:34 +0100

    Reupload a fixed 1.20.5 branch with DL patches

--- common/src/main/java/org/vivecraft/client_vr/VRState.java
+++ common/src/main/java/org/vivecraft/client_vr/VRState.java
@@ -40,12 +40,6 @@ public class VRState {
             vrInitialized = true;
             ClientDataHolderVR dh = ClientDataHolderVR.getInstance();
             if (dh.vrSettings.stereoProviderPluginID == VRSettings.VRProvider.OPENVR) {
-                // make sure the lwjgl version is the right one
-                // TODO: move this into the init, does mean all callocs need to be done later
-                // check that the right lwjgl version is loaded that we ship the openvr part of
-                if (!Version.getVersion().startsWith("3.3.3")) {
-                    throw new RenderConfigException("VR Init Error", Component.translatable("vivecraft.messages.rendersetupfailed", I18n.get("vivecraft.messages.invalidlwjgl", Version.getVersion(), "3.3.2"), "OpenVR_LWJGL"));
-                }
 
                 dh.vr = new MCOpenVR(Minecraft.getInstance(), dh);
             } else {
